fd2aa3e51f7f7ff916025a8b6750fac60894082a
infodevestudent <infodev.infodev@gmail.com>
2023-05-27 07:01:20 -0300

    up2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let dataCampo = document.getElementById("data");
+dataCampo.addEventListener("input", function () {
+    dataCampo.value = dataCampo.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{4})\d+?$/, '$1');
+});
