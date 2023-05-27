140a82aead0e9965cbe0d22101ab31dbf66b3714
infodevestudent <infodev.infodev@gmail.com>
2023-05-27 08:18:37 -0300

    up6

--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@ dataCampo.addEventListener("input", function () {
         .replace(/(\d{2})(\d)/, '$1/$2')
         .replace(/(\d{4})\d+?$/, '$1');
 });
+
+
+function autoFormatarTelCel(campo) {
+    let caracteres = campo.value.length;
+    let valor = campo.value;
+
+    if (caracteres == 2) {
+        campo.value = '(0' + valor + ') ';
+    }
+
+    if (caracteres == 7) {
+        campo.value = valor + '-';
+    }
+
+    if (caracteres == 12) {
+        campo.value = valor + '-';
+    }
+}
+function autoFormatarTelFix(campo) {
+    let caracteres = campo.value.length;
+    let valor = campo.value;
+
+    if (caracteres == 2) {
+        campo.value = '(0' + valor + ') ';
+    }
+
+    if (caracteres == 10) {
+        campo.value = valor + '-';
+    }
+}
